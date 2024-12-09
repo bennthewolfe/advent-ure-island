089c9205f43f979090ed8ab4b943dccb7b2e0d1b
Benn Wolfe <bennwolfe@rocketcentral.com>
2024-12-09 17:19:01 -0500

    Updated shared resources.

--- resources/aoc-YYYY-day-X.1.js
+++ resources/aoc-YYYY-day-X.1.js
@@ -1,8 +1,12 @@
+// Run with node --inspect [filename] to debug
+
 fs = require('fs');
 
 const fullData = JSON.parse(fs.readFileSync('source.json','utf-8'));
 const shortData = fullData.slice(0,10);
 
-let data = fullData;
+// Uncomment the fullData overwrite to run against the full dataset
+let data = shortData;
+//data = fullData;
 
 console.table(data);
--- resources/aoc-YYYY-day-X.md
+++ resources/aoc-YYYY-day-X.md
@@ -1,5 +1,5 @@
-# Advent of Code - 2022 Day 6
-https://adventofcode.com/2022/day/7
+# Advent of Code - 2024 Day 6
+https://adventofcode.com/2024/day/6
 
 | Property | Score |
 |-------|---------|
