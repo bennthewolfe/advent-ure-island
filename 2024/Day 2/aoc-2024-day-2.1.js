6ecfe3493a63ca3b5ee231e80adacf580ea3a098
Benn Wolfe <bennwolfe@rocketcentral.com>
2024-12-09 19:04:58 -0500

    Incomplete Day 2.2

---
+++ 2024/Day 2/aoc-2024-day-2.1.js
@@ -0,0 +1,58 @@
+// Run with node --inspect [filename] to debug
+// Run with node --watch index.js to watch for changes and run
+
+fs = require('fs');
+
+const fullData = JSON.parse(fs.readFileSync('source.json','utf-8'));
+const shortData = fullData.slice(0,10);
+
+// Uncomment the fullData overwrite to run against the full dataset
+let data = shortData;
+data = fullData;
+
+// console.table(data);
+
+/* Strategy
+- FAIL : If I sort an increasing array and it changes
+- FAIL : If I sort a decreasing array and it changes
+*/
+
+let successArray = [];
+
+data.forEach((element) => {
+    // determine sort order and normalized to asc
+    if (element[0] < element[element.length-1]) {
+        // ascending
+    } else if (element.sort()[0] < element[element.length-1]) {
+        // descending
+    } else {
+        // equal
+        successArray.push(false);
+        return false;
+    }
+
+    if (! assessRules(element) ) { successArray.push(false); return false; }
+
+    // Pass
+    successArray.push(true);
+    return true;   
+});
+
+// console.table(successArray);
+
+let successCount = successArray.filter((element) => element === true).length;
+console.log(`Success Count: ${successCount}`);
+
+function assessRules (element) {
+    return element.every(
+        (val,index,arr) => {
+            if (index > 0) {
+                let diff = val - arr[index-1];
+                if (diff < 1 || diff > 3) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    )
+}
---
+++ 2024/Day 2/aoc-2024-day-2.md
@@ -0,0 +1,37 @@
+# Advent of Code - 2024 Day 2
+https://adventofcode.com/2024/day/2
+
+| Property | Score |
+|-------|---------|
+|*Difficulty* :|⭐⭐⭐⭐⭐|
+|*Fun* :|⭐⭐⭐⭐⭐|
+|*Time* :| Long (~3 hrs)|
+|*Overall* :|⭐⭐⭐⭐⭐|
+
+⭐⭐⭐⭐⭐ 🌟🌟🌟🌟🌟 ☆☆☆☆☆ ★★★★★
+
+## Part 1
+---
+*Backstory* : Checking on the Red-Nosed reactor to see if the reports on the levels are safe.
+
+*Strategy Selected* : 
+
+## Part 2
+---
+*Backstory* : The problem dampener allows for one failure.
+
+*Strategy Selected* : Change `every()` function to account for one failure.
+
+## Core Concepts :
+---
+*Puzzle Keys* :
+- [Array Sort](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort) - MDN Web Docs - Array Sort
+- [Compare Arrays](https://www.freecodecamp.org/news/how-to-compare-arrays-in-javascript/) - Free Code Camp - Compare Arrays
+- [Array.every()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every) - MDN Web Docs - Every loop
+- 
+
+## Alternative Strategies
+---
+- **Trap** : Array.sort() modifies the item. Old way : Use .slice() to make a copy before sorting.  New way : Use toSorted().
+- **Trap** : Array.every() looks for a return and stops on falsiness.
+- I tried to use the fact that arrays will or won't match after sorting as an efficiency play.
